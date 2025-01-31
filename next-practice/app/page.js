900f51484bcb1570c1d4eaddc2854c479d8ef247
김영서 <dudtj926@naver.com>
2025-01-31 17:10:36 +0900

    페이지 link로 연결하기
    페이지 link로 연결하기

--- next-practice/app/page.js
+++ next-practice/app/page.js
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1>Hello Next.js</h1>;
+  return (
+    <>
+      <h1>Hello Next.js</h1>
+      <Link href="/news">Go to News</Link>
+    </>
+  );
 }
