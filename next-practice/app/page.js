a0a7be6a6a7d7cf48db452ef2eba847ab5a6d063
김영서 <dudtj926@naver.com>
2025-01-31 20:58:07 +0900

    prisma 설치, 데이터 패칭해서 화면에 그리기
    prisma 설치, 데이터 패칭해서 화면에 그리기

--- next-practice/app/page.js
+++ next-practice/app/page.js
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { prisma } from "./db";
 
-export default function Home() {
+export default async function Home() {
+  //await prisma.todo.create({ data: { title: "test" } });
+  const todos = await prisma.todo.findMany();
+  console.log(todos);
   return (
     <>
       <h1>Hello Next.js</h1>
       <Link href="/news">Go to News</Link>
+      {todos.map((todo) => (
+        <h1 key={todo.id}>{todo.title}</h1>
+      ))}
     </>
   );
 }
